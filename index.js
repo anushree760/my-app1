2a9da2cf5e47ea8d83858e2ba63ff3ba691bf1a5
unknown <anushree760>
2025-05-15 18:25:42 +0530

    Change greeting to 'Hello from Render'

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// index.js
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from Heroku!'));
+app.get("/", (req, res) => {
+  res.send("Hello from Render");
+});
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
